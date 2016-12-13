1f9f0e5bc925cb62de63b9b4e3848cc7d11cfeec
小矮子&&流浪狗 <liuguoxi@chd.edu.cn>
2016-12-13 18:50:52 +0800

    Main function for calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# Calculator
+#
+# This is the main function of our calculator.
+#
+import ply.lex as lex
+import ply.yacc as yacc
+import calctokens
+import calcgrammar
+import calcinterp
+
+case_1 = '''
+    function max(a, b){
+        if (a > b){
+            return a;
+        } else{
+            return b;
+        }
+    }
+    out(max(3, 4));
+'''
+
+calclexer = lex.lex(module=calctokens)
+calcparser = yacc.yacc(module=calcgrammar, tabmodule='parsetab')
+ast = calcparser.parse(case_1, lexer=calclexer)
+print ast
+print calcinterp.interpret(ast)
